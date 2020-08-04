cb575f3e78638854e4dd523b6b7e2355190c4cd2
buitam <tambt@ttc-solutions.com>
2020-08-04 17:47:32 +0700

    design login VC

--- TTCTraining/ViewControllers/LoginVC/LoginViewController.swift
+++ TTCTraining/ViewControllers/LoginVC/LoginViewController.swift
@@ -9,25 +9,45 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    var controller: LoginPageView?
-    static var index:Int = 0
-    
-    @IBOutlet weak var contentView: UIView!
-    @IBOutlet weak var segmentControl: UISegmentedControl!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        controller = storyboard!.instantiateViewController(withIdentifier: "LoginPageView") as! LoginPageView
-        addChild(controller!)
-        contentView.addSubview(controller!.view)
-        controller!.didMove(toParent: self)
+ 
+        @IBOutlet weak var topConstraintHeight: NSLayoutConstraint!
+       
+        @IBOutlet weak var logoTopConstraint: NSLayoutConstraint!
+
+        @IBOutlet weak var topTravelMatesConstraint: NSLayoutConstraint!
+        @IBAction func hideSigInPop(_ sender: Any) {
+            topConstraintHeight.constant = 1000
+            logoTopConstraint.constant = 252
+            topTravelMatesConstraint.constant = 446
+                        lblTravelMates.font = UIFont(name:"Noteworthy", size: 35.0)
+            lblWelcome.isHidden = false
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: .curveEaseOut, animations:{ self.view.layoutIfNeeded()}, completion: nil)
+            print("clicked")
+        }
+        @IBOutlet weak var lblWelcome: UILabel!
+        @IBOutlet weak var lblTravelMates: UILabel!
+        @IBAction func showLoginPop(_ sender: Any) {
+            topConstraintHeight.constant = 180
+            logoTopConstraint.constant = 50
+            topTravelMatesConstraint.constant = 175
+      
+            lblTravelMates.font = UIFont(name:"Noteworthy", size: 25.0)
+           
+            lblWelcome.isHidden = true
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: .curveEaseOut, animations:{ self.view.layoutIfNeeded()}, completion: nil)
+            print("clicked")
+        }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            topConstraintHeight.constant = 1000
+            logoTopConstraint.constant = 252
+            topTravelMatesConstraint.constant = 446
+            
+            lblWelcome.isHidden = false
+                lblTravelMates.font = UIFont(name:"Noteworthy", size: 35.0)
+            // Do any additional setup after loading the view.
+        }
+
+
     }
-    
-    @IBAction func btnCancel(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    @IBAction func segment(_ sender: Any) {
-        controller?.didChangePage(segmentControl.selectedSegmentIndex)
-    }
-}
+
